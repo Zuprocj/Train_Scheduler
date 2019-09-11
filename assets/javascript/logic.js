e6e291ff4b585187ea8f2e05036785a91c09c764
zuprocj <jmdcorpuz@gmail.com>
2019-09-11 15:06:46 -0700

    worked on js. got time to display correctly. working on clearing form after user input.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,31 +4,50 @@ var config = {
     databaseURL: "https://uw-bootcamp-191d8.firebaseio.com",
     storageBucket: "uw-bootcamp-191d8.appspot.com",
 };
-    firebase.initializeApp(config);
+firebase.initializeApp(config);
     
-    var dataRef = firebase.database();
+var database = firebase.database();
     
-    var train = "";
-    var destination = "";
-    var time = "";
-    var frequency = "";
+$("#train").submit(function(event) {
     
-    $("#addTrain").on("click", function(event) {
-    
-        event.preventDefault();
-    
-        train = $("#trainInput").val().trim();
-        destination = $("#destinationInput").val().trim();
-        time = $("#timeInput").val().trim();
-        frequency = $("#frequencyInput").val().trim();
-    
-        dataRef.ref().push({
-            
-            train: train,
-            destination: destination,
-            time: time,
-            frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-});
+    var trainName = $("#trainInput").val().trim();
+    var destinationName = $("#destinationInput").val().trim();
+    var timeStart = moment($("#timeInput").val().trim(), "HH:mm").format("X");
+    var frequencyRate = $("#frequencyInput").val().trim();
+        
+    var newTrain = {
+    name: trainName,
+    destination: destinationName,
+    start: timeStart,
+    frequency: frequencyRate,
+};
 
+    database.ref().push(newTrain);
+    
+    $("#train-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+    
+    return false;
+});
+    
+database.ref().on("child_added", function(childSnapshot, prevChildKey){
+        
+    var trainName = childSnapshot.val().name;
+    var destinationName = childSnapshot.val().destination;
+    var timeStart = childSnapshot.val().start;
+    var frequencyRate = childSnapshot.val().frequency;
+        
+    var firstTimeConverted = moment(timeStart, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % frequencyRate;
+    var tMinutesTillTrain = frequencyRate - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var formattedTime = moment(nextTrain).format("HH:mm");
+
+     $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destinationName + "</td><td>" + frequencyRate 
+     + "</td><td>" + formattedTime +"</td><td>" + tMinutesTillTrain + "</td>");
+     
+ });
